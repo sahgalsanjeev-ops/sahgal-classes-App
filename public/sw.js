efc3c7a107ddbbf4eb34f4cf250a45e4dd249b2d
sanjeev sahgal <sahgal.sanjeev@gmail.com>
2026-04-05 17:29:07 +0530

    Updated sw.js for better caching

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,14 @@
+const CACHE_NAME = 'sahgal-classes-v1';
+const urlsToCache = ['/'];
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installed');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(event.request))
-    );
-  });
-  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
