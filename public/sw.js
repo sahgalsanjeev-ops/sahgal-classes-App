07c07654f92cce3f516e5b937ca33f93a9dc5705
sanjeev sahgal <sahgal.sanjeev@gmail.com>
2026-04-05 16:35:37 +0530

    PWA setup with icons and manifest

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+  });
+  
